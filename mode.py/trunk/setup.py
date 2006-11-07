26230c231b2e37c9892dedce7579449f273c6ab5
whit537 <whit537@25ad2b5d-ad21-0410-9345-3bd1a98bad77>
2006-11-07 22:02:06 +0000

    approaching release

---
+++ mode.py/trunk/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+
+classifiers = [
+    'Development Status :: 4 - Beta'
+  , 'Intended Audience :: Developers'
+  , 'License :: OSI Approved :: BSD License'
+  , 'Natural Language :: English'
+  , 'Operating System :: MacOS :: MacOS X'
+  , 'Operating System :: Microsoft :: Windows'
+  , 'Operating System :: POSIX'
+  , 'Operating System :: Unix'
+  , 'Programming Language :: Python'
+  , 'Topic :: Software Development :: Libraries :: Python Modules'
+   ]
+
+
+setup( name='mode.py'
+     , version='1.0'
+     , package_dir = {'':'src'}
+     , py_modules=['mode']
+     , description = ( 'mode.py models the life-cycle of an application as a '
+                     + 'series of four modes.'
+                      )
+     , author = 'Chad Whitacre'
+     , author_email = 'chad@zetaweb.com'
+     , url = 'http://www.zetadev.com/software/mode.py/'
+     , classifiers = classifiers
+      )
